40ae73d8e63bb161f81e4daa5c49fd89a98baca9
RattaphoomT <rattaphoom.t@kkumail.com>
2025-02-14 15:24:22 +0700

    update

--- app/Http/Middleware/PersonnelMiddleware.php
+++ app/Http/Middleware/PersonnelMiddleware.php
@@ -12,19 +12,10 @@ class PersonnelMiddleware
     public function handle(Request $request, Closure $next)
     {
 
-        // ตรวจสอบว่า user ไม่ได้ล็อกอิน
-        if (!Auth::check()) {
-            // ถ้าไม่ล็อกอิน ให้ไปหน้า login
-            return redirect()->route('login')->with('error', 'กรุณาเข้าสู่ระบบ');
+        if (Auth::check() && Auth::user()->user_role_iduser_role == 1) {
+            return $next($request);
         }
 
-        // ตรวจสอบ Role ของผู้ใช้
-        if (Auth::user()->user_role_iduser_role != 1) {
-            // ถ้าไม่ใช่ เจ้าหน้าที่ (user_role_iduser_role != 1) ให้ไปหน้า login
-            return redirect()->route('login')->with('error', 'คุณไม่มีสิทธิ์เข้าถึงหน้านี้');
-        }
-
-        // ถ้าไม่มีปัญหาใดๆ ให้ไปหน้าต่อไป
-        return $next($request);
+        return redirect()->route('login')->with('error', 'กรุณาเข้าสู่ระบบด้วยผู้ใช้ระดับ เจ้าหน้าที่');
     }
 }
